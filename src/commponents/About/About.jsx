3ee7761abb36a18fe0ad1d76b336f12ec4cf8749
ovaqvado <likeriklohhg@gmail.com>
2024-04-07 12:39:53 +0300

    Banner

--- src/commponents/About/About.jsx
+++ src/commponents/About/About.jsx
@@ -4,8 +4,8 @@ import Style from "./About.module.css";
 function About() {
   return (
     <div className={Style.mains}>
-      <img src="https://www.shutterstock.com/image-vector/female-external-genitalia-labelled-diagram-260nw-2292627393.jpg" />
-      <h1 className={Style.sex}>За сто рублей цветок будет в анус</h1>
+      {/* <img src="https://www.shutterstock.com/image-vector/female-external-genitalia-labelled-diagram-260nw-2292627393.jpg" />
+      <h1 className={Style.sex}>За сто рублей цветок будет в анус</h1> */}
     </div>
   );
 }
--- src/commponents/Banner/Banner.jsx
+++ src/commponents/Banner/Banner.jsx
@@ -1,7 +1,43 @@
 import React from "react";
-
+import Style from "./Banner.module.css";
+import flowerImg from "../../img/flower_img.png";
+import money from "../../img/money_img.png";
+import deliveryCar from "../../img/deliveryCar.png";
 function Banner() {
-  return <div>Banner</div>;
+  return (
+    <div className={Style.banner}>
+      <p className={Style.title_banner}>Быстрая доставка цветов в Сочи</p>
+      <p className={Style.title_banner_text}>
+        Соберем букет и привезем за 60 минут.
+        <br /> Бесплатная доставка по городу от 3000 руб.
+      </p>
+      <div className={Style.banner_info}>
+        <div className={Style.banner_card}>
+          <p className={Style.text_info}>
+            Букеты собираются
+            <br />
+            опытными флористами
+          </p>
+          <img src={flowerImg} alt="" />
+        </div>
+        <div className={Style.banner_card}>
+          <p className={Style.text_info}>
+            Скидка 7% на первый
+            <br />
+            заказ
+          </p>
+          <img src={money} alt="" />
+        </div>
+        <div className={Style.banner_card}>
+          <p className={Style.text_info}>
+            Быстрая достака <br />
+            24 часа
+          </p>
+          <img className={Style.info_img} src={deliveryCar} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Banner;
